540ac53abcb19a589a7de10710fa35b5e9b46ea5
cody-ray <crank.20@osu.edu>
2018-07-16 02:36:41 -0700

    🦖

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ $(document).ready(()=>{
                 console.log(e);
                 var classname = e.className
                 var prob = e.probability.toFixed(4)
-                var prediction = "<h2 class='center'>Class name: "+classname+"</h2> +\
-                 <br> + <h2 class='center'>Probability: "+prob+"</h2>" 
+                var prediction = "<h2 class='center'>Class name: "+classname+"</h2>\
+                 <br> <h2 class='center'>Probability: "+prob+"</h2>" 
                 $("#Prediction").append(prediction);
             });
             
